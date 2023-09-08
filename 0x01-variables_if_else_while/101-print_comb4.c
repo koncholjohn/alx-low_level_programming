eb6c2180d5bcd57b03f03b3555ba22c96302c89d
koncholjohn <deaction14@gmail.com>
2023-09-08 07:55:26 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,38 +1,42 @@
 #include <stdio.h>
 
 /**
- * main - function to print combination of two integers
+ * main - print all possible difference combination of three digits.
  *
- * Return: 0 on success
+ * Return : 0 success
  */
 
 int main(void)
 {
 	int i = '0';
 	int j = '0';
+	int k = '0';
 
-	while (i <= '9')
+	while (i <= '7')
 	{
-		while (j <= '9')
+		while (j <= '8')
 		{
-			if (!(i > j) || i == j)
+			while (k <= '9')
 			{
-				putchar(i);
-				putchar(j);
-				if (i == '8' && j == '9')
+				if (i < j && j < k)
 				{
-					putchar('\n');
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					if (!(i == '7' && j == '8' && k == '9'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
-				else
-				{
-					putchar(',');
-					putchar(' ');
-				}
+				k++;
 			}
+			k = '0';
 			j++;
 		}
 		j = '0';
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
