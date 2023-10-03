059b4e871a0742f1898b10de93855e7d4db1491b
koncholjohn <deaction14@gmail.com>
2023-10-03 01:27:08 -0700

    If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -31,7 +31,7 @@ int **alloc_grid(int width, int height)
 		if (gridout[i] == NULL)
 		{
 			for (i--; i >= 0; i--)
-			free(gridout);
+			free(gridout)[i];
 			free(gridout[i]);
 			return (NULL);
 		}
