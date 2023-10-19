eb20480c79a56add2c0102616bb51f77eff43c17
koncholjohn <deaction14@gmail.com>
2023-10-19 07:21:19 -0700

    The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+void ComeFirstbyCTC(void)__attribute__((constructor));
+/**
+ * ComeFirst - function that run the main function
+ */
+void ComeFirstbyCTC(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n|");
+}
