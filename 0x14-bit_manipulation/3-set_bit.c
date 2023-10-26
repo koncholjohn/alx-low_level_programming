d8d94ca767deb174d01764c46614e69e3111fb5c
koncholjohn <deaction14@gmail.com>
2023-10-26 03:00:00 -0700

    11

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -11,7 +11,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	unsigned int mask;
 
 	if (index > sizeof(unsigned int) * 8)
-		return (-10);
+		return (-1);
 	mask = 1;
 	mask = mask << index;
 	*n = ((*n) | mask);
