34785beb0f974a690dee1cd7d2a8ac4b8c66d44d
koncholjohn <deaction14@gmail.com>
2023-12-22 01:30:27 -0800

    commit changes

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -6,25 +6,27 @@
  */
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *node, *temp;
+	unsigned long int i;
+	hash_node_t *node, *temp;
 
-    if (ht == NULL)
+	if (ht == NULL)
         return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        node = ht->array[i];
-        while (node != NULL)
-        {
-            temp = node;
-            node = node->next;
-            free(temp->key);
-            free(temp->value);
-            free(temp);
-        }
-    }
+	for (i = 0; i < ht->size; i++)
+		node = ht->array[i];
+	{
+	while (node != NULL)
+       {
+	       temp = node;
+	       node = node->next;
+	       free(temp->key);
+	       free(temp->value);
+	       free(temp);
+       }
+	}
 
-    free(ht->array);
-    free(ht);
+	free(ht->array);
+	free(ht);
 }
+    
+
